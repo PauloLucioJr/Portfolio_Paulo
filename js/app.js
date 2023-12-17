3731c3f2e8f26064e8652292713f78d9e23ee1cc
PauloLucioJr <paulo.lucio.jr@hotmail.com>
2023-12-17 19:26:03 -0300

    criada páginas portfólio e contato

---
+++ js/app.js
@@ -0,0 +1,7 @@
+const botaoMobile = document.querySelector(".botao-mobile");
+
+const nav = document.querySelector(".nav");
+
+botaoMobile.addEventListener("click", () => nav.classList.toggle("active"));
+
+
